83b151c9169e8c8690860443375d9b8fe4c2f7a7
unodeepak <unodeepak1@gmail.com>
2024-02-11 00:25:37 +0530

    add swap two number

--- DSA_with_JS/bitsManipulation.js
+++ DSA_with_JS/bitsManipulation.js
@@ -1,11 +1,23 @@
 /* Lists of questions for bits manipulation using JavaScript */
 
+
+
+
+/* WAP to swap two number */
+let a = 100;
+let b = 200;
+
+a = a ^ b;
+b = a ^ b;
+a = a ^ b;
+console.log({ a, b });
+
 /* WAP to find the given number is even of odd */
 const checkEvenOdd = (num) => {
   if (num & 1) {
     return "Odd";
   } else {
-    return "Even"
+    return "Even";
   }
-}
-console.log(checkEvenOdd(11))
+};
+// console.log(checkEvenOdd(11))
